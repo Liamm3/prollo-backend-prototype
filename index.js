c450afa539686b2e8fedd27914743d8586059921
lwesy <lw@esyoil.com>
2018-03-15 09:41:09 +0100

    add body-parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000 || process.env.PORT;
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.send({
