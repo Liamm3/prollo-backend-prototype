1ee17500f9600f8a2dd0b67e277ca16c99e43e9b
lwesy <lw@esyoil.com>
2018-03-15 10:08:13 +0100

    add board routes, controller and model. connect to mongodb

---
+++ board/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const Router = express.Router;
+
+const router = new Router();
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+require('./db/mongoose');
+
+const boardRoutes = require('./board/routes');
 
 const app = express();
 const port = 3000 || process.env.PORT;
@@ -8,6 +11,7 @@ const port = 3000 || process.env.PORT;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', boardRoutes);
 
 app.get('/', (req, res) => {
   res.send({
