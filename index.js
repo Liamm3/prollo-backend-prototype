ac2c83a45ed8c7c53b23aee23069497bbb567680
lwesy <lw@esyoil.com>
2018-03-15 09:37:52 +0100

    add morgan middleware

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const logger = require('morgan');
 
 const app = express();
 const port = 3000 || process.env.PORT;
+
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.send({
