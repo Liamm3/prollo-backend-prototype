1265dbe77591a194051f945c90e375883c4053dd
lwesy <lw@esyoil.com>
2018-03-15 14:21:44 +0100

    add lists to seed

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,13 @@
 const {ObjectID} = require('mongodb');
 
 const Board = require('../board/model');
+const List = require('../list/model');
 
 const boardOneId = new ObjectID();
 const boardTwoId = new ObjectID();
+
+const listOneId = new ObjectID();
+const listTwoId = new ObjectID();
 
 const boards = [
   {
@@ -16,13 +20,34 @@ const boards = [
   }
 ];
 
+const lists = [
+  {
+    _id: listOneId,
+    name: 'List 1',
+    _board: boardOneId
+  },
+  {
+    _id: listTwoId,
+    name: 'List 2',
+    _board: boardTwoId
+  }
+];
+
 const populateBoards = async done => {
   await Board.remove({});
   await Board.insertMany(boards);
   return done();
 };
 
+const populateLists = async done => {
+  await List.remove({});
+  await List.insertMany(lists);
+  return done();
+};
+
 module.exports = {
   boards,
-  populateBoards
+  populateBoards,
+  lists,
+  populateLists
 };
